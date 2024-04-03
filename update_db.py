f928146beb6de995b3c39cba64fb699f973cb105
Fortune Adekogbe <fortuneadekogbe@gmail.com>
2024-04-03 13:41:59 +0100

    Updated script to use scraper.py

--- update_db.py
+++ update_db.py
@@ -1,7 +1,8 @@
 import pymongo
 from dotenv import load_dotenv, find_dotenv
 import os
-from datetime import datetime, timedelta, date
+from datetime import timedelta
+from scraper import *
 
 load_dotenv(find_dotenv())
 
@@ -13,34 +14,9 @@ client = pymongo.MongoClient(MONGODB_URI)
 db = client['power_track']
 
 # DATE ON WEBSITE
-date_str = "SUN 31ST MARCH, 2024"
-_, date_str = date_str.split(maxsplit=1)
+_, date_str = website_date.split(maxsplit=1)
 date_str = date_str.replace("ST", "").replace("ND", "").replace("RD", "").replace("TH", "")
 website_date = datetime.strptime(date_str, "%d %B, %Y").date()
-
-today_data = {
-    'date': datetime.strptime(str(date.today()), '%Y-%m-%d'), 
-    'metadata': {'source': 'power.gov.ng'},
-    'Grid @ 06:00 (MW)': 4110.19,
-    'Frequency @ 06:00 (Hz)': 50.17, 
-}
-
-yesterday_data = {
-    'Peak Generation (MW)': 4303.0,
-    'Off-Peak Generation (MW)': 3694.95,
-    'Energy Generated (MWh)': 96131.84,
-    'Energy Sent Out (MWh)': 94991.1,
-    'Frequency @ Peak Generation (Hz)': 50.23,
-    'Frequency @ Off-Peak Generation (Hz)': 50.62,
-    'Peak Generation Hour': 17.0,
-    'Off-Peak Generation Hour': 17.0,
-    'Highest Frequency (Hz)': 50.76,
-    'Lowest Frequency (Hz)': 49.08,
-    'Hour @ Highest Frequency': 17.0,
-    'Hour @ Lowest Frequency': 16.0,
-    'Highest Voltage': '357.00kV @ Gwagwalada TS @ 14:00Hrs',
-    'Lowest Voltage': '300.00kV @ Yola & Gombe TS @ 01:00 & 01:00Hrs'
-}
 
 # add to the data for yesterday depending on how updated the website is.
 yesterday = date.today() - timedelta(days=1)
@@ -49,6 +25,7 @@ if website_date == yesterday:
     filter = {"date": datetime.strptime(str(yesterday), '%Y-%m-%d')}
     update = {"$set": yesterday_data}
     result = db.gridlines.update_one(filter, update)
+    print(result.modified_count)
     if (result.modified_count) == 0:
         yesterday_data = {
             "date": datetime.strptime(str(yesterday), '%Y-%m-%d'),
@@ -61,3 +38,4 @@ if website_date == yesterday:
     # add data for today if it doesn't already exist
     result = db.gridlines.insert_one(today_data)
     print(result)
+print("Done.")
